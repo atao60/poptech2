bef2e05c033ed85dbd5e70064b2e8fa99be6de42
atao <p.m.raoul@gmail.com>
2020-12-21 20:15:24 +0100

    ci: no base href rewrite

--- scully.popsuite-site.config.prod.ts
+++ scully.popsuite-site.config.prod.ts
@@ -1,38 +1,15 @@
-import { RouteTypes, ScullyConfig, setPluginConfig } from '@scullyio/scully';
-import { MinifyHtml } from 'scully-plugin-minify-html';
-import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
-
-import { env } from 'process';
-
-const BASE_DIR = env.BASE_DIR || './dist/static/';
-const BASE_HREF = env.BASE_HREF || '/';
-
-const defaultPostRenderers = [baseHrefRewrite, 'seoHrefOptimise', MinifyHtml];
-
-function getBaseHref () {
-  const baseHrefParts = [BASE_HREF];
-  if (!BASE_HREF.startsWith('/')) baseHrefParts.unshift('/');
-  if (!BASE_HREF.endsWith('/')) baseHrefParts.push('/');
-  const baseHref = baseHrefParts.join('');
-  console.log("Scully config with base href: ", baseHref);
-  return baseHref;
-}
-
-setPluginConfig('md', { enableSyntaxHighlighting: true });
-
-setPluginConfig(baseHrefRewrite, { href: getBaseHref () });
+import { RouteTypes, ScullyConfig } from '@scullyio/scully';
 
 export const config: ScullyConfig = {
-  projectRoot: './src',
-  projectName: 'popsuite-site',
-  outDir: BASE_DIR,
-  defaultPostRenderers,
-  routes: {
-    '/blog/:slug': {
-      type: RouteTypes.contentFolder,
-      slug: {
-        folder: './blog'
+    projectRoot: './src',
+    projectName: 'popsuite-site',
+    outDir: './dist/static',
+    routes: {
+      '/blog/:slug': {
+        type: RouteTypes.contentFolder,
+        slug: {
+          folder: './blog'
+        }
       }
     }
-  }
-};
+  };
