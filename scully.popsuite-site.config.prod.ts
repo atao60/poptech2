2323e41d2a8d46394286c18ad9c81dc4c12128ef
atao <p.m.raoul@gmail.com>
2020-12-21 21:01:04 +0100

    ci: trial with @scullyio/scully-plugin-base-href-rewrite

--- scully.popsuite-site.config.prod.ts
+++ scully.popsuite-site.config.prod.ts
@@ -1,15 +1,21 @@
-import { RouteTypes, ScullyConfig } from '@scullyio/scully';
+import { RouteTypes, setPluginConfig, ScullyConfig } from '@scullyio/scully';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
+
+const defaultPostRenderers = [baseHrefRewrite];
+
+setPluginConfig(baseHrefRewrite, { href: 'https://atao60.github.io/poptech2/' });
 
 export const config: ScullyConfig = {
-    projectRoot: './src',
-    projectName: 'popsuite-site',
-    outDir: './dist/static',
-    routes: {
-      '/blog/:slug': {
-        type: RouteTypes.contentFolder,
-        slug: {
-          folder: './blog'
-        }
+  projectRoot: './src',
+  projectName: 'popsuite-site',
+  outDir: './dist/static',
+  defaultPostRenderers,
+  routes: {
+    '/blog/:slug': {
+      type: RouteTypes.contentFolder,
+      slug: {
+        folder: './blog'
       }
     }
-  };
+  }
+};
